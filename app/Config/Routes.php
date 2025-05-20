9e684c102b9ad8901ef02ad6292d363265b6d1e1
harrylbi <harrylbi014@gmail.com>
2025-05-20 14:14:14 +0700

    update CRUD

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,13 +6,22 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/produk', 'Home::index', ['filter' => 'auth']);
+// $routes->get('/produk', 'Home::index', ['filter' => 'auth']);
 $routes->get('/', 'Home::index', ['filter' => 'auth']);
 
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login', ['filter' => 'Redirect']); 
 $routes->get('logout', 'AuthController::logout');
 
-$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+
+$routes->get('contact', 'FaqController::index', ['filter' => 'auth']);
 $routes->get('faq', 'FaqController::index', ['filter' => 'auth']);
+$routes->get('profil', 'FaqController::index', ['filter' => 'auth']);
--- app/Controllers/ProdukController.php
+++ app/Controllers/ProdukController.php
@@ -2,14 +2,85 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\ProductModel;
 
 class ProdukController extends BaseController
 {
+    protected $product; 
+
+    public function __construct()
+    {
+        $this->product = new ProductModel();
+    }
+
     public function index()
     {
-        return view('v_produk');
-
+        $data['product'] = $this->product->findAll();
+        return view('v_produk', $data);
     }
-}                                                                                                                                                                                                                                                                                     
+
+    public function create()
+    {
+        $dataFoto = $this->request->getFile('foto');
+
+        $dataForm = [
+            'nama'       => $this->request->getPost('nama'),
+            'harga'      => $this->request->getPost('harga'),
+            'jumlah'     => $this->request->getPost('jumlah'),
+            'created_at' => date("Y-m-d H:i:s")
+        ];
+
+        if ($dataFoto && $dataFoto->isValid()) {
+            $fileName = $dataFoto->getRandomName();
+            $dataFoto->move('img/', $fileName);
+            $dataForm['foto'] = $fileName;
+        }
+
+        $this->product->insert($dataForm);
+
+        return redirect('produk')->with('success', 'Data Berhasil Ditambah');
+    } 
+
+    public function edit($id)
+    {
+        $dataProduk = $this->product->find($id);
+
+        $dataForm = [
+            'nama'       => $this->request->getPost('nama'),
+            'harga'      => $this->request->getPost('harga'),
+            'jumlah'     => $this->request->getPost('jumlah'),
+            'updated_at' => date("Y-m-d H:i:s")
+        ];
+
+        if ($this->request->getPost('check') == 1) {
+            if (!empty($dataProduk['foto']) && file_exists("img/" . $dataProduk['foto'])) {
+                unlink("img/" . $dataProduk['foto']);
+            }
+
+            $dataFoto = $this->request->getFile('foto');
+
+            if ($dataFoto && $dataFoto->isValid()) {
+                $fileName = $dataFoto->getRandomName();
+                $dataFoto->move('img/', $fileName);
+                $dataForm['foto'] = $fileName;
+            }
+        }
+
+        $this->product->update($id, $dataForm);
+
+        return redirect('produk')->with('success', 'Data Berhasil Diubah');
+    }
+
+    public function delete($id)
+    {
+        $dataProduk = $this->product->find($id);
+
+        if (!empty($dataProduk['foto']) && file_exists("img/" . $dataProduk['foto'])) {
+            unlink("img/" . $dataProduk['foto']);
+        }
+
+        $this->product->delete($id);
+
+        return redirect('produk')->with('success', 'Data Berhasil Dihapus');
+    }
+}
